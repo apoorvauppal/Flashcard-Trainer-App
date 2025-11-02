469686e9ae97a76bd5a44d1b965fac01fcb8d48b
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:15:09 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/FloatingButton.tsx
+++ project/FlashcardsTrainer/src/components/FloatingButton.tsx
@@ -12,6 +12,11 @@ const styles = StyleSheet.create({
     fab:{
         position: "absolute",
         right:18, 
+        bottom: 26,
+        width: 56,
+        height:56,
+        borderRadius: 28
+        backgroundColor: "#007AFF",
         
     }
 })
