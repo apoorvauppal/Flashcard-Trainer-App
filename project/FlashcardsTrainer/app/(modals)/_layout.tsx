9ca6be03c3f6517fce95a776c4117a38e3a221a1
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:26:39 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(modals)/_layout.tsx
+++ project/FlashcardsTrainer/app/(modals)/_layout.tsx
@@ -4,7 +4,7 @@ import {Stack} from "expo-router";
 export default function ModalsLayout(){
     return(
         <Stack screenOptions={{presentation = "modal"}}>
-
+            <Stack.Screen name="create-deck" options={{title:}}
         </Stack>
     )
 }
