4d02f52a33a89afc1ca05f26d6522e8a47acc98c
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 21:47:53 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/DeckCard.tsx
+++ project/FlashcardsTrainer/src/components/DeckCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Deck } from '../contexts/FlashContext';
+import { TouchableOpacity } from 'react-native';
 
 export default function DeckCard ({deck, onPress}: {deck: Deck; onPress:() => void}){
     return(
-        <TouchableOpac
+        <TouchableOpacity onPress = {onPress} style={StyleS}
     )
 }
