e2d0566b90874c44a21cb0f37d984daddc122649
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:23:39 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/_layout.tsx
+++ project/FlashcardsTrainer/app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React from "react";;
+import {Tabs} from "expo-router";
 
 export default function TabsLayout(){
     return(
-        <Tabs>
+        <Tabs 
+        screenOptions>
         </Tabs>
     )
 }
