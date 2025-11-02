e237bd83a07a1886e3159d928fe644adbf344211
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 21:47:23 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/DeckCard.tsx
+++ project/FlashcardsTrainer/src/components/DeckCard.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
+import { Deck } from '../contexts/FlashContext';
 
-export default function DeckCard ({deck, onPress}:)
+export default function DeckCard ({deck, onPress}: {deck: Deck; onPress:()})
