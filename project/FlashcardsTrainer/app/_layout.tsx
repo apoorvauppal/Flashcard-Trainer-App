a9d8a3994d9a77a6adc512e20ca7d0b346747f3d
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 23:05:26 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/_layout.tsx
+++ project/FlashcardsTrainer/app/_layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import {Slot} from "expo-router";
+import { Stack } from "expo-router";
 import { FlashProvider } from "../src/contexts/FlashContext";
 
-export default function RootLayout(){
-    return(
-        <FlashProvider>
-            <Slot />
-        </FlashProvider>
-    );
+export default function RootLayout() {
+  return (
+    <FlashProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(modals)"
+          options={{ presentation: "modal", headerShown: false }}
+        />
+      </Stack>
+    </FlashProvider>
+  );
 }
