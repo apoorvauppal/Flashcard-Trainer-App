3a5dc20eb3f7d1a781c17f603198b2f01e79ea0e
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 21:49:38 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/DeckCard.tsx
+++ project/FlashcardsTrainer/src/components/DeckCard.tsx
@@ -20,5 +20,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.05, 
         elevation: 2,
     },
-    title:{fontSize: 16, fontWeight: "700"}
+    title:{fontSize: 16, fontWeight: "700"}, 
+    count: {marginTop: 6, color: "#666"}
 })
