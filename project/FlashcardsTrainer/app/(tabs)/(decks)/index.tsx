c4bda51248eaf21492f5cb513a5cb56e4bbeb962
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:29:39 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/(decks)/index.tsx
+++ project/FlashcardsTrainer/app/(tabs)/(decks)/index.tsx
@@ -1,9 +1,9 @@
 import { useRouter } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import { useFlash } from "../../../src/contexts/FlashContext";
 
 export default function DecksScreen(){
     const router = useRouter();
     const {decks} = useFlash();
-    const [query, ]
+    const [query, setQuery] = useState
 }
