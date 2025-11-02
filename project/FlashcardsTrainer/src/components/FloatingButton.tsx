59de0c1f240fa886edde1accdf7e53e1732d9f92
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:16:54 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/FloatingButton.tsx
+++ project/FlashcardsTrainer/src/components/FloatingButton.tsx
@@ -3,7 +3,7 @@ import{StyleSheet} from "react-native";
 
 export default function FloatingButton({onPress}: {onPress: () => void}){
     return(
-        <TouchableOpacity onPress></TouchableOpacity>
+        <TouchableOpacity onPress = {onpress} style = {styles.fab}}></TouchableOpacity>
     )
 }
 
