5414cb7a19601b981a0cf198bfd33846bf0e303e
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:05:24 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/SearchBar.tsx
+++ project/FlashcardsTrainer/src/components/SearchBar.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function SearchBar({
-    value, onChangeText, placeholder,}
+    value, 
+    onChangeText, 
+    placeholder,}:{
+        value: string, onChangeText
+    }
 )
