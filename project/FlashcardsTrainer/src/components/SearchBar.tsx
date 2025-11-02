c5115bc5ee79274186f6c0e4ec7320d4823b8fa9
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:07:24 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/SearchBar.tsx
+++ project/FlashcardsTrainer/src/components/SearchBar.tsx
@@ -23,6 +23,7 @@ const styles = StyleSheet.create({
         padding: 10, 
         borderRadius:8,
         borderWidth:1,
-        
+        borderColor:"#ddd",
+        backgroundColor:"#fff",
     }
 })
