5560efde87762e43f84fb34d184380222b831d27
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 22:27:09 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(modals)/_layout.tsx
+++ project/FlashcardsTrainer/app/(modals)/_layout.tsx
@@ -5,7 +5,7 @@ export default function ModalsLayout(){
     return(
         <Stack screenOptions={{presentation = "modal"}}>
             <Stack.Screen name="create-deck" options={{title:"Create Deck"}}/>
-            
+            <Stack.Screen name="create-card" options={{title:"Add Card"}}/>
         </Stack>
     )
 }
