df3d88057b0adbad7e358777efad21b0fb53d11a
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 19:57:20 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/layout.tsx
+++ project/FlashcardsTrainer/app/layout.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import {FlashProvider} from "../src/contents/FlashContext"
 
 export default function RootLayout(){
     return(
-  
+        <FlashProvider>
+
+        </FlashProvider>
     )
 }
