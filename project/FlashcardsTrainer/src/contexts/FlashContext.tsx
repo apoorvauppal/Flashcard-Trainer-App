edc4b689977b8810a2676f558bd0cc703ec49b84
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 20:10:35 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/contexts/FlashContext.tsx
+++ project/FlashcardsTrainer/src/contexts/FlashContext.tsx
@@ -28,5 +28,5 @@ type FlashContextType = {
 const FlashContext = createContext<FlashContextType | undefined>(undefined);
 
 function generateId(prefix = ""){
-    return '${prefix}${Date.now().toString(36)}-$Math.random().toString(36).slice((2,8'
+    return '${prefix}${Date.now().toString(36)}-$Math.random().toString(36).slice((2,8)}';
 }
