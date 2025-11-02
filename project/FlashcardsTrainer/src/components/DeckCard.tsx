d5a19bdb65386be4089a27bc92bce397fbd99607
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-02 21:47:38 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/src/components/DeckCard.tsx
+++ project/FlashcardsTrainer/src/components/DeckCard.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import { Deck } from '../contexts/FlashContext';
 
-export default function DeckCard ({deck, onPress}: {deck: Deck; onPress:()})
+export default function DeckCard ({deck, onPress}: {deck: Deck; onPress:() => void}){
+    return(
+        <TouchableOpac
+    )
+}
