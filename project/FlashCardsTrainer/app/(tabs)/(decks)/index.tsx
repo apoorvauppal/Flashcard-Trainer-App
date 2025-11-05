1190a6a0b26e96eee274f9730ad185969ebf9107
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:05:03 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/(decks)/index.tsx
+++ project/FlashCardsTrainer/app/(tabs)/(decks)/index.tsx
@@ -1,30 +1,13 @@
-// import React from 'react';
-// import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
 
-// export default function DecksScreen(){
-//     return(
-//         <View style = {{flex:1, 
-//         justifyContent: "center",
-//         alignItems:"center"}}>
-//             <Text style = {{fontSize:24}}>Decks List</Text>
-//         </View>
-//     );
-// }
-
-import { useContext } from "react";
-import { Text, View } from "react-native";
-import { FlashContext } from "../../../src/contexts/FlashContext";
-
-export default function DecksScreen() {
-  const { decks } = useContext(FlashContext);
-
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Decks:</Text>
-      {Object.values(decks).map((deck) => (
-        <Text key={deck.id}>• {deck.title}</Text>
-      ))}
-    </View>
-  );
+export default function DecksScreen(){
+    return(
+        <View style = {{flex:1, 
+        justifyContent: "center",
+        alignItems:"center"}}>
+            <Text style = {{fontSize:24}}>Decks List</Text>
+        </View>
+    );
 }
