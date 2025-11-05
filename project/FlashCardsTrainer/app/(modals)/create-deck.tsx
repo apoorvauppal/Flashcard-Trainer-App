93a67cec733ef59c6bfef8a18818fd46a48adde9
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:13:48 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(modals)/create-deck.tsx
+++ project/FlashCardsTrainer/app/(modals)/create-deck.tsx
@@ -1,6 +1,7 @@
 import React, {useContext, useState} from 'react' 
 import { FlashContext } from '../../src/contexts/FlashContext';
 import {useRouter} from 'expo-router'
+import { View } from 'react-native';
 
 export default function CreateDeckModal(){
     const [title, setTitle] = useState("");
@@ -11,5 +12,9 @@ export default function CreateDeckModal(){
         if(!title.trim()) return;
         createDeck(title.trim());
         router.back();
-    }
+    };
+
+    return(
+        <View></View>
+    )
 }
