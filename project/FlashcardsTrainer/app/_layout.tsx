b34c0a457a7d0a11cd72b246461dadad729f2551
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 19:56:45 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/_layout.tsx
+++ project/FlashcardsTrainer/app/_layout.tsx
@@ -1,15 +1,16 @@
 // app/_layout.tsx
 import React from "react";
 import { Stack } from "expo-router";
+import { FlashProvider } from "../src/contexts/FlashContext";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* main UI */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      {/* modals group will be defined in a separate route group, route files determine stack */}
-      <Stack.Screen name="(modals)/create-deck" options={{ presentation: "modal", title: "Create Deck" }} />
-      <Stack.Screen name="(modals)/create-card" options={{ presentation: "modal", title: "Create Card" }} />
-    </Stack>
+    <FlashProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(modals)/create-deck" options={{ presentation: "modal", title: "Create Deck" }} />
+        <Stack.Screen name="(modals)/create-card" options={{ presentation: "modal", title: "Create Card" }} />
+      </Stack>
+    </FlashProvider>
   );
 }
