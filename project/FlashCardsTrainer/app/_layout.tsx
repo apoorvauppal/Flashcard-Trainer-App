0ecf00d96e1f1135e9356848f8829564f0822310
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 20:21:31 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/_layout.tsx
+++ project/FlashCardsTrainer/app/_layout.tsx
@@ -1,8 +1,11 @@
+import { Stack } from 'expo-router';
 import React from 'react';
 
 
 export default function RootLayout(){
     return(
-        
+        <Stack>
+            <Stack.Screen name = "(tabs)"
+        </Stack>
     )
 }
