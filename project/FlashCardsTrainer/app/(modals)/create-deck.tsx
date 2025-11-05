352c409d036a4f1a179b54c4fb04adf3cc787e8d
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:12:33 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(modals)/create-deck.tsx
+++ project/FlashCardsTrainer/app/(modals)/create-deck.tsx
@@ -1,6 +1,8 @@
 import React, {useContext, useState} from 'react' 
+import { FlashContext } from '../../src/contexts/FlashContext';
 
 export default function CreateDeckModal(){
     const [title, setTitle] = useState("");
-    const {createDeck} = useContext
+    const {createDeck} = useContext(FlashContext)
+    const router = useRouter();
 }
