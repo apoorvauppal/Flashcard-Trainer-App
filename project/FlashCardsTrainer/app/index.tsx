bf3e2d1fa4044fb2ecd5b077e96328532be912d3
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 20:48:32 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/index.tsx
+++ project/FlashCardsTrainer/app/index.tsx
@@ -1,17 +1,5 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function HomeScreen() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff",
-      }}
-    >
-      <Text style={{ fontSize: 24 }}>Flashcards Trainer</Text>
-      <Text>Expo Router is working!</Text>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="/(tabs)/(decks)" />;
 }
