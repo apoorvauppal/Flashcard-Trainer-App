82d5cbdad4f186a6c8b1091c70e107b74846454c
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:17:03 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(modals)/create-deck.tsx
+++ project/FlashCardsTrainer/app/(modals)/create-deck.tsx
@@ -31,6 +31,10 @@ const styles = StyleSheet.create({
         marginBottom: 8,
     },
     input:{
-        
+        borderWidth: 1,
+        borderColor:"#ccc",
+        borderRadius: 8,
+        padding:12,
+        marginBottom:16
     }
 })
