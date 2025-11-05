1f27b3bcd0fd94f8b6c8a36eb1ab01ef8417f7f8
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:12:18 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(modals)/create-deck.tsx
+++ project/FlashCardsTrainer/app/(modals)/create-deck.tsx
@@ -1,5 +1,6 @@
-import React from 'react' 
+import React, {useContext, useState} from 'react' 
 
 export default function CreateDeckModal(){
     const [title, setTitle] = useState("");
+    const {createDeck} = useContext
 }
