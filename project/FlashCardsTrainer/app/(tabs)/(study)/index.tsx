5f3564d09cc3027dde5a1ecb96a25e410a9138ff
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 20:27:32 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/(study)/index.tsx
+++ project/FlashCardsTrainer/app/(tabs)/(study)/index.tsx
@@ -5,6 +5,7 @@ import { View } from 'react-native';
 export default function StudyScreen(){
     return(
         <View style={{flex:1,
+            justifyContent: "center",
             
         }}></View>
     )
