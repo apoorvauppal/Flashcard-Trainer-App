5b11486a2a8d7bbd26199be873072b48ade3d6b9
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 20:21:46 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/_layout.tsx
+++ project/FlashCardsTrainer/app/_layout.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function RootLayout(){
     return(
         <Stack>
-            <Stack.Screen name = "(tabs)"
+            <Stack.Screen name = "(tabs)" options = {{headerShown: false}}
         </Stack>
     )
 }
