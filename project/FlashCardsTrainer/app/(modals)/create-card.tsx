a235b0f3ba0f255095e56714b538606008a3b178
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:25:18 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(modals)/create-card.tsx
+++ project/FlashCardsTrainer/app/(modals)/create-card.tsx
@@ -4,4 +4,5 @@ import React, {useState} from 'react'
 export default function CreateCardModal(){
     const [question, setQuestion] = useState("");
     const [answer, setAnswer] = useState("");
+    const {addCardToDeck} = useContext(FlashContext);
 }
