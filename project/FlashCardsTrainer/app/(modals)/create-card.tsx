967e8664064b4ddbcedace29c94696826882a73f
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:25:48 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(modals)/create-card.tsx
+++ project/FlashCardsTrainer/app/(modals)/create-card.tsx
@@ -1,9 +1,12 @@
 import React, {useState, useContext} from 'react'
 import { FlashContext } from '../../src/contexts/FlashContext';
+import { useRouter } from 'expo-router';
 
 
 export default function CreateCardModal(){
     const [question, setQuestion] = useState("");
     const [answer, setAnswer] = useState("");
     const {addCardToDeck} = useContext(FlashContext);
+    const router = useRouter();
+    const {deckId} = useSearchParams
 }
