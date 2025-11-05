754beff755bc7389be544d3949e2ed29c8dcdbc0
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:41:49 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/(decks)/deck/[id].tsx
+++ project/FlashCardsTrainer/app/(tabs)/(decks)/deck/[id].tsx
@@ -16,7 +16,7 @@ export default function DeckDetailScreen(){
         <View style={{flex:1, padding:16}}>
             <Button
                 title="Add Card"
-                onPress={() => router.push(`/(modals)`)}
+                onPress={() => router.push(`/(modals)/create-card?deckId=$`)}
             >
 
             </Button>
