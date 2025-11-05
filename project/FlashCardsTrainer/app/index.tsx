20f162462799f6b11d6b4e406cb063f0400fde8b
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 20:10:46 +0000

    --- Auto Save ---

---
+++ project/FlashCardsTrainer/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontSize: 24 }}>Flashcards Trainer 🧠</Text>
+      <Text>Expo Router is working!</Text>
+    </View>
+  );
+}
