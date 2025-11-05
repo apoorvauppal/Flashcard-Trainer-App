976b9aa6084023acf19a7259b135fca8cec14e0a
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:30:48 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/src/contexts/FlashContext.tsx
+++ project/FlashCardsTrainer/src/contexts/FlashContext.tsx
@@ -16,7 +16,7 @@ type Deck = {
 type FlashContextType = {
     decks: Record<string, Deck>;
     createDeck: (title: string) => void;
-    addCardToDeck: (deckId: string, card:{question:string; answer: string})
+    addCardToDeck: (deckId: string, card:{question:string; answer: string}) => void;
 };
 
 export const FlashContext = createContext<FlashContextType>({
