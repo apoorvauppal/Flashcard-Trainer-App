8c4bed92a6a259a40dd31894c5c8186311071dcf
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:39:04 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/(decks)/deck/[id].tsx
+++ project/FlashCardsTrainer/app/(tabs)/(decks)/deck/[id].tsx
@@ -1,9 +1,11 @@
-import { useSearchParams } from 'expo-router/build/hooks'
+import { useRouteInfo, useRouter, useSearchParams } from 'expo-router/build/hooks'
 import React, { useContext } from 'react'
 import { FlashContext } from '../../../../src/contexts/FlashContext';
 
 
 export default function DeckDetailScreen(){
     const{deckId} = useSearchParams<{deckId: string}>();
-    const{decks} = useContext(FlashContext)
+    const{decks} = useContext(FlashContext);
+    const router = useRouter();
+    
 }
