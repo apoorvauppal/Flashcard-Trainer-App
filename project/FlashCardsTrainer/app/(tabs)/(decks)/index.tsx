e278f4e64356b34a250cad6f804c7e4721228815
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 21:04:33 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/(decks)/index.tsx
+++ project/FlashCardsTrainer/app/(tabs)/(decks)/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// import React from 'react';
+// import { View, Text } from 'react-native';
 
 
-export default function DecksScreen(){
-    return(
-        <View style = {{flex:1, 
-        justifyContent: "center",
-        alignItems:"center"}}>
-            <Text style = {{fontSize:24}}>Decks List</Text>
-        </View>
-    );
-}
+// export default function DecksScreen(){
+//     return(
+//         <View style = {{flex:1, 
+//         justifyContent: "center",
+//         alignItems:"center"}}>
+//             <Text style = {{fontSize:24}}>Decks List</Text>
+//         </View>
+//     );
+// }
