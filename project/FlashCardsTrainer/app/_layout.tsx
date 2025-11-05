9bec391ff6f76c16882039bf5426b2208501b4fe
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-05 20:22:16 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/_layout.tsx
+++ project/FlashCardsTrainer/app/_layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout(){
     return(
         <Stack>
             <Stack.Screen name = "(tabs)" options = {{headerShown: false}} />
-            <Stack.Screen name = "(tabs)" options = {{headerShown: false}} />
-            <Stack.Screen name = "(tabs)" options = {{headerShown: false}} />
+            {/* <Stack.Screen name = "(tabs)" options = {{headerShown: false}} /> */}
+            {/* <Stack.Screen name = "(tabs)" options = {{headerShown: false}} /> */}
 
         </Stack>
     )
