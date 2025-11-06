63fbe49a381c3954b641d178d852f9837453a3f7
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-06 03:20:08 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/_layout.tsx
+++ project/FlashCardsTrainer/app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function TabsLayout(){
     return(
-        <Tabs>
+        <Tabs screenOptions={{headerShown: false}}>
             <Tabs.Screen name='(decks)' options={{title:"Decks", tabBarLabel: "Decks", tabBarIcon: () => "🗂️"}}/>
             <Tabs.Screen name='(study)' options={{title:"Study", tabBarLabel: "Study", tabBarIcon: () => "⭐"}}/>
     
