a5186a1f01947e5af8b30d0c4e90004a00cbd0ae
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-06 03:02:57 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/_layout.tsx
+++ project/FlashCardsTrainer/app/_layout.tsx
@@ -1,24 +1,24 @@
-import { Stack } from 'expo-router';
-import React from 'react';
+// import { Stack } from 'expo-router';
+// import React from 'react';
 
 
-export default function RootLayout(){
-    return(
-        <Stack>
-            <Stack.Screen name = "(tabs)" options = {{headerShown: false}} />
-            <Stack.Screen name = "(modals)/create-deck" options = {{presentation: "modal", title: "New Deck"}} />
-            <Stack.Screen name = "(modals)/create-card" options = {{presentation: "modal", title: "New Card"}} />
-        </Stack>
-    )
-}
-
-// import { Stack } from "expo-router";
-// import { FlashProvider } from "../src/contexts/FlashContext";
-
-// export default function RootLayout() {
-//   return (
-//     <FlashProvider>
-//       <Stack screenOptions={{ headerShown: false }} />
-//     </FlashProvider>
-//   );
+// export default function RootLayout(){
+//     return(
+//         <Stack>
+//             <Stack.Screen name = "(tabs)" options = {{headerShown: false}} />
+//             <Stack.Screen name = "(modals)/create-deck" options = {{presentation: "modal", title: "New Deck"}} />
+//             <Stack.Screen name = "(modals)/create-card" options = {{presentation: "modal", title: "New Card"}} />
+//         </Stack>
+//     )
 // }
+
+import { Stack } from "expo-router";
+import { FlashProvider } from "../src/contexts/FlashContext";
+
+export default function RootLayout() {
+  return (
+    <FlashProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </FlashProvider>
+  );
+}
