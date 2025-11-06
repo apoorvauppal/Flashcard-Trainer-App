47b46751284978a02ee922bd5ce211d0f2e95772
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-06 02:52:12 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/_layout.tsx
+++ project/FlashCardsTrainer/app/(tabs)/_layout.tsx
@@ -5,7 +5,7 @@ export default function TabsLayout(){
     return(
         <Tabs>
             <Tabs.Screen name='(decks)' options={{title:"Decks", tabBarIcon: () => "🗂️"}}/>
-            <Tabs.Screen name="(study)" options ={{ title: "Study", tabBarIcon: () => "⭐"}}/>
+            {/* <Tabs.Screen name="(study)" options ={{ title: "Study", tabBarIcon: () => "⭐"}}/> */}
         </Tabs>
     )
 }
