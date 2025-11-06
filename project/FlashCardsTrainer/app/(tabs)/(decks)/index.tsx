a296519e45aea610961ccda63c6510f5a5ee27f9
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-06 02:57:42 +0000

    --- Auto Save ---

--- project/FlashCardsTrainer/app/(tabs)/(decks)/index.tsx
+++ project/FlashCardsTrainer/app/(tabs)/(decks)/index.tsx
@@ -22,7 +22,9 @@ export default function DecksScreen() {
             <Text style = {{fontSize:20, marginTop:20, marginBottom:10}}>Favorite Cards</Text>
             <FlatList data={favorites} keyExtractor={(item) => item.id}
             renderItem={({item}) => (
-              <View
+              <View>
+                
+              </View>
             )}
               >
               
